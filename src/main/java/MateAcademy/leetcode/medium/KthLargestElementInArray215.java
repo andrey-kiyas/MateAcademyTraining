6d465f624403b5ffc2993e37bf1133a65227f89b
ANDREY <andrey.kiyas@gmail.com>
2024-01-21 19:08:30 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/KthLargestElementInArray215.java
@@ -0,0 +1,27 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array
+ */
+
+public class KthLargestElementInArray215 {
+
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));          // 5
+        System.out.println(findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4)); // 4
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
---
+++ src/main/java/MateAcademy/leetcode/medium/SumOfSubarrayMinimums907.java
@@ -0,0 +1,52 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/sum-of-subarray-minimums
+ */
+
+public class SumOfSubarrayMinimums907 {
+
+    public static void main(String[] args) {
+        System.out.println(sumSubarrayMins(new int[]{3, 1, 2, 4}));        // Вывод: 17
+        System.out.println(sumSubarrayMins(new int[]{11, 81, 94, 43, 3})); // Вывод: 444
+    }
+
+    public static int sumSubarrayMins(int[] arr) {
+        int MOD = 1_000_000_007;
+        int n = arr.length;
+        int[] left = new int[n]; // left[i] - индекс ближайшего элемента слева, меньшего arr[i]
+        int[] right = new int[n]; // right[i] - индекс ближайшего элемента справа, меньшего arr[i]
+
+        // Используем стек для хранения индексов элементов в порядке возрастания
+        // и обновляем left и right для каждого элемента
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear(); // Очищаем стек для использования в правом проходе
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[i] <= arr[stack.peek()]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        long result = 0;
+
+        // Для каждого элемента arr[i], добавляем arr[i] * количество подмассивов, в которых arr[i] - минимум
+        for (int i = 0; i < n; i++) {
+            result = (result + (long) arr[i] * (i - left[i]) * (right[i] - i) % MOD) % MOD;
+        }
+
+        return (int) result;
+    }
+}
