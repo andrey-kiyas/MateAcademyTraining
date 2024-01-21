19e052797fa67abab5f31343618782c01e5edba3
ANDREY <andrey.kiyas@gmail.com>
2024-01-21 21:29:42 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/HouseRobber198.java
@@ -0,0 +1,40 @@
+package MateAcademy.leetcode.medium;
+
+/**
+ * https://leetcode.com/problems/house-robber
+ */
+
+public class HouseRobber198 {
+
+    public static void main(String[] args) {
+//        System.out.println(rob(new int[]{1, 2, 3, 1}));    // 1 + 3 = 4
+//        System.out.println(rob(new int[]{2, 7, 9, 3, 1})); // 2 + 9 + 1 = 12
+        System.out.println(rob(new int[]{1, 9, 2, 8, 3, 7, 4})); // 9 + 8 + 7 = 24
+    }
+
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+    public static int rob2(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                result += nums[i];
+            }
+        }
+        return result;
+    }
+}
