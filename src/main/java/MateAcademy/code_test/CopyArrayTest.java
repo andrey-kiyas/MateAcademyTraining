19bc65328faa03ad6bb41298accc200f3c49b3d1
ANDREY <andrey.kiyas@gmail.com>
2024-02-11 15:32:58 +0200

    working commit

---
+++ src/main/java/MateAcademy/code_test/CopyArrayTest.java
@@ -0,0 +1,14 @@
+package MateAcademy.code_test;
+
+public class CopyArrayTest {
+    public static void main(String[] args) {
+        int[] x = {1, 2, 3, 4};
+        int[] y = x;
+
+        x = new int[2];
+
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(y[i] + " ");
+        }
+    }
+}
