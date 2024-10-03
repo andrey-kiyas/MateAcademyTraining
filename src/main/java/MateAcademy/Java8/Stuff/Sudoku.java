1b2504f2f97713de3b3604c46ef10d85915b2c17
Andrey Kiyas <andrey.kiyas@gmail.com>
2024-10-03 21:40:24 +0300

    working commit

--- src/main/java/MateAcademy/Java8/Stuff/Sudoku.java
+++ src/main/java/MateAcademy/Java8/Stuff/Sudoku.java
@@ -9,7 +9,13 @@ public class Sudoku {
 
     public void printSudoku() {
         for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.println("-------------------------");
+            }
             for (int j = 0; j < 9; j++) {
+                if (j % 3 == 0 && j != 0) {
+                    System.out.print(" |  ");
+                }
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
