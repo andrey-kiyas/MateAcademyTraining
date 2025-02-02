625405ccb2a81cce059f57d9964fc0a268e35d36
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-02-02 10:23:41 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/easy/LC3151SpecialArrayI.java
@@ -0,0 +1,36 @@
+package MateAcademy.leetcode.easy;
+
+public class LC3151SpecialArrayI {
+
+    public static void main(String[] args) {
+        System.out.println(isArraySpecial(new int[]{1}));
+        System.out.println(isArraySpecial(new int[]{2, 1, 4}));
+        System.out.println(isArraySpecial(new int[]{4, 3, 1, 6}));
+    }
+
+    public static boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i] % 2) == (nums[i - 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isArraySpecial2(int[] nums) {
+        if (nums.length > 1) {
+
+            boolean even = nums[0] % 2 == 0;
+            boolean curr = false;
+
+            for (int i = 1; i < nums.length; i++) {
+                curr = nums[i] % 2 == 0;
+                if (even == curr) {
+                    return false;
+                }
+                even = curr;
+            }
+        }
+        return true;
+    }
+}
