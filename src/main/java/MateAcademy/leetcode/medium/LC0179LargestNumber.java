4adf628d2b66347cacabff8da3c126bb8e904571
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-02-06 17:44:55 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/LC0179LargestNumber.java
@@ -0,0 +1,25 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/largest-number
+ */
+
+public class LC0179LargestNumber {
+
+    public static void main(String[] args) {
+        System.out.println(largestNumber(new int[]{3, 30, 34, 5, 9}));
+        System.out.println(largestNumber(new int[]{0, 0}));
+    }
+
+    public static String largestNumber(int[] nums) {
+        String result = Arrays.stream(nums)
+                .mapToObj(String::valueOf)
+                .sorted((a, b) -> (b + a).compareTo(a + b))
+                .reduce((a, b) -> a + b)
+                .orElse("0");
+
+        return result.startsWith("0") ? "0" : result;
+    }
+}
