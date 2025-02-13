b5b08112c6892d6cf9a4d5ecaeeb7a7bcae16462
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-02-13 18:55:08 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/easy/LC3110ScoreOfAString.java
@@ -0,0 +1,20 @@
+package MateAcademy.leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/score-of-a-string
+ */
+
+public class LC3110ScoreOfAString {
+
+    public static void main(String[] args) {
+        System.out.println(scoreOfString("hello"));
+    }
+
+    public static int scoreOfString(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            result += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return result;
+    }
+}
