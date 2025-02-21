64e7067a1c067b144fb3dba3f4c9a53f153037c6
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-02-21 22:20:55 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/LC1261FindElementsInAContaminatedBinaryTree.java
@@ -0,0 +1,37 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree
+ */
+
+public class LC1261FindElementsInAContaminatedBinaryTree {
+    public static void main(String[] args) {
+        FindElements findElements = new FindElements(new TreeNode(-1, null, new TreeNode(-1)));
+
+        System.out.println(findElements.find(1)); // false
+        System.out.println(findElements.find(2)); // true
+    }
+}
+
+class FindElements {
+    private Set<Integer> values = new HashSet<>();
+
+    public FindElements(TreeNode root) {
+        recoverTree(root, 0);
+    }
+
+    private void recoverTree(TreeNode node, int val) {
+        if (node == null) return;
+        node.val = val;
+        values.add(val);
+        recoverTree(node.left, 2 * val + 1);
+        recoverTree(node.right, 2 * val + 2);
+    }
+
+    public boolean find(int target) {
+        return values.contains(target);
+    }
+}
