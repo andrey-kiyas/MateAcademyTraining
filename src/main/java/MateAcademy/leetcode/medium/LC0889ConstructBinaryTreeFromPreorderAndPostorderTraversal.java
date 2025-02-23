84fbcc9dd1f8e8f075a89eb76d352c06ed837caa
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-02-23 22:22:06 +0200

    working commit

--- src/main/java/MateAcademy/leetcode/medium/LC0889ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
+++ src/main/java/MateAcademy/leetcode/medium/LC0889ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -7,8 +7,7 @@ import java.util.Queue;
 
 public class LC0889ConstructBinaryTreeFromPreorderAndPostorderTraversal {
     public static void main(String[] args) {
-        System.out.println(
-                treeToList(
+        System.out.println(treeToList(
                         constructFromPrePost(
                                 new int[]{1, 2, 4, 5, 3, 6, 7},
                                 new int[]{4, 5, 2, 6, 7, 3, 1}))); // [1,2,3,4,5,6,7]
