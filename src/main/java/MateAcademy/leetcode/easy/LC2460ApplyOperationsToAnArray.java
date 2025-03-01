43923472742a1dbb0a0d307334253fdb62240bef
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-01 21:31:43 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/easy/LC2460ApplyOperationsToAnArray.java
@@ -0,0 +1,34 @@
+package MateAcademy.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/apply-operations-to-an-array
+ */
+
+public class LC2460ApplyOperationsToAnArray {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(applyOperations(new int[]{1, 2, 2, 1, 1, 0}))); // [1, 4, 2, 0, 0, 0]
+        System.out.println(Arrays.toString(applyOperations(new int[]{0, 1})));             // [1, 0]
+    }
+
+    public static int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int[] result = new int[n];
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
