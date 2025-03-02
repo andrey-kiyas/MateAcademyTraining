4933132c66d55b8281de8f1349ef200505fd5625
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-02 22:03:00 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/easy/LC2570MergeTwo2DArraysBySummingValues.java
@@ -0,0 +1,47 @@
+package MateAcademy.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values
+ */
+
+public class LC2570MergeTwo2DArraysBySummingValues {
+    public static void main(String[] args) {
+//        Input: nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
+//        Output: [[1,6],[2,3],[3,2],[4,6]]
+//
+//        Input: nums1 = [[2,4],[3,6],[5,5]], nums2 = [[1,3],[4,3]]
+//        Output: [[1,3],[2,4],[3,6],[4,3],[5,5]]
+
+
+        System.out.println(Arrays.deepToString(mergeArrays(
+                new int[][]{{1, 2}, {2, 3}, {4, 5}},
+                new int[][]{{1, 4}, {3, 2}, {4, 1}})));     // [[1, 4], [2, 6], [4, 10]]
+        System.out.println(Arrays.deepToString(mergeArrays(
+                new int[][]{{2, 4}, {3, 6}, {5, 5}},
+                new int[][]{{1, 3}, {4, 3}})));             // [[1, 3], [2, 4], [3, 6], [4, 3], [5, 5]]
+    }
+
+    public static int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int[] pair : nums1) {
+            map.put(pair[0], map.getOrDefault(pair[0], 0) + pair[1]);
+        }
+
+        for (int[] pair : nums2) {
+            map.put(pair[0], map.getOrDefault(pair[0], 0) + pair[1]);
+        }
+
+        int[][] result = new int[map.size()][2];
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            result[index++] = new int[]{entry.getKey(), entry.getValue()};
+        }
+
+        return result;
+    }
+}
