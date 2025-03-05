14ef67fed98b984ef57d85bdb244cb27c9c1302a
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-05 21:54:47 +0200

    working commit

--- src/main/java/MateAcademy/leetcode/medium/LC2579CountTotalNumberOfColoredCells.java
+++ src/main/java/MateAcademy/leetcode/medium/LC2579CountTotalNumberOfColoredCells.java
@@ -8,6 +8,7 @@ public class LC2579CountTotalNumberOfColoredCells {
     public static void main(String[] args) {
         System.out.println(coloredCells(1)); // 1
         System.out.println(coloredCells(2)); // 5
+        System.out.println(coloredCells(3)); // 13
     }
 
     public static long coloredCells(int n) {
