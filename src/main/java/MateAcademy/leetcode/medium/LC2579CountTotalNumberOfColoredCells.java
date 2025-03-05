f12305e74e73dd94680308df339f5b3c533bd0f1
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-05 21:52:15 +0200

    working commit

--- src/main/java/MateAcademy/leetcode/medium/LC2579CountTotalNumberOfColoredCells.java
+++ src/main/java/MateAcademy/leetcode/medium/LC2579CountTotalNumberOfColoredCells.java
@@ -6,19 +6,8 @@ package MateAcademy.leetcode.medium;
 
 public class LC2579CountTotalNumberOfColoredCells {
     public static void main(String[] args) {
-
-//        Input: n = 1
-//        Output: 1
-//        Explanation: After 1 minute, there is only 1 blue cell, so we return 1.
-//        Example 2:
-//
-//        Input: n = 2
-//        Output: 5
-
-
         System.out.println(coloredCells(1));
         System.out.println(coloredCells(2));
-
     }
 
     public static long coloredCells(int n) {
