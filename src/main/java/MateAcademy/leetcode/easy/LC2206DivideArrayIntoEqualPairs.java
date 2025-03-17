7006e684ddcd44ce34c0f53f44c91d67d72949c1
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-17 12:06:37 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/easy/LC2206DivideArrayIntoEqualPairs.java
@@ -0,0 +1,27 @@
+package MateAcademy.leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/divide-array-into-equal-pairs
+ */
+
+public class LC2206DivideArrayIntoEqualPairs {
+    public static void main(String[] args) {
+        System.out.println(divideArray(new int[]{3, 2, 3, 2, 2, 2})); // true
+        System.out.println(divideArray(new int[]{1, 2, 3, 4})); // false
+    }
+
+    public static boolean divideArray(int[] nums) {
+        int[] count = new int[501];
+
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        for (int c : count) {
+            if (c % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
