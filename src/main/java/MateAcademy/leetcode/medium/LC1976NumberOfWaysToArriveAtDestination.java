d080eadce095b2d14e61fabebba667198b7b6a85
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-23 09:10:49 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/LC1976NumberOfWaysToArriveAtDestination.java
@@ -0,0 +1,66 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * https://leetcode.com/problems/number-of-ways-to-arrive-at-destination
+ */
+
+public class LC1976NumberOfWaysToArriveAtDestination {
+    public static void main(String[] args) {
+        int[][] roads1 = {{0,6,7},{0,1,2},{1,2,3},{1,3,3},{6,3,3},{3,5,1},{6,5,1},{2,5,1},{0,4,5},{4,6,2}};
+        int[][] roads2 = {{1,0,10}};
+
+        System.out.println(countPaths(7, roads1)); // Output: 4
+        System.out.println(countPaths(2, roads2)); // Output: 1
+    }
+
+    public static int countPaths(int n, int[][] roads) {
+        final long inf = Long.MAX_VALUE / 2;
+        final int mod = (int) 1e9 + 7;
+        long[][] g = new long[n][n];
+        for (var e : g) {
+            Arrays.fill(e, inf);
+        }
+        for (var r : roads) {
+            int u = r[0], v = r[1], t = r[2];
+            g[u][v] = t;
+            g[v][u] = t;
+        }
+        g[0][0] = 0;
+        long[] dist = new long[n];
+        Arrays.fill(dist, inf);
+        dist[0] = 0;
+        long[] f = new long[n];
+        f[0] = 1;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            int t = -1;
+            for (int j = 0; j < n; ++j) {
+                if (!vis[j] && (t == -1 || dist[j] < dist[t])) {
+                    t = j;
+                }
+            }
+            vis[t] = true;
+            for (int j = 0; j < n; ++j) {
+                if (j == t) {
+                    continue;
+                }
+                long ne = dist[t] + g[t][j];
+                if (dist[j] > ne) {
+                    dist[j] = ne;
+                    f[j] = f[t];
+                } else if (dist[j] == ne) {
+                    f[j] = (f[j] + f[t]) % mod;
+                }
+            }
+        }
+        return (int) f[n - 1];
+    }
+}
