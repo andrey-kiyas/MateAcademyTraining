45b79bc616a5e797e56410e20d069b3b3a572274
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-03-30 21:27:53 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/LC0763PartitionLabels.java
@@ -0,0 +1,32 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC0763PartitionLabels {
+    public static void main(String[] args) {
+        System.out.println(partitionLabels("ababcbacadefegdehijhklij")); // [9,7,8]
+        System.out.println(partitionLabels("eccbbbbdec"));               // [10]
+    }
+
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        int[] lastIndex = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
