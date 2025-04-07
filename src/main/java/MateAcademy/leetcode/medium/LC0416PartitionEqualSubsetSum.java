a7ca5cf83d5ef51939a519673421662190917df3
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-04-07 09:01:36 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/medium/LC0416PartitionEqualSubsetSum.java
@@ -0,0 +1,32 @@
+package MateAcademy.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/partition-equal-subset-sum
+ */
+
+public class LC0416PartitionEqualSubsetSum {
+    public static void main(String[] args) {
+        System.out.println(canPartition(new int[]{1, 5, 11, 5})); // true
+        System.out.println(canPartition(new int[]{1, 2, 3, 5}));  // false
+    }
+
+    public static boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
