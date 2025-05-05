d7cf3e3e41fa3997380b83bb3288db11a715b31d
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-05-05 21:24:39 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC02Medium/LC0790DominoAndTrominoTiling.java
@@ -0,0 +1,31 @@
+package MateAcademy.leetcode.LC02Medium;
+
+/**
+ * https://leetcode.com/problems/domino-and-tromino-tiling
+ */
+
+public class LC0790DominoAndTrominoTiling {
+    public static void main(String[] args) {
+        System.out.println(numTilings(3));  // Output: 5
+        System.out.println(numTilings(1));  // Output: 1
+    }
+
+    public static int numTilings(int n) {
+        int MOD = 1_000_000_007;
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        long sum = dp[0];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] % MOD + dp[i - 3]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
