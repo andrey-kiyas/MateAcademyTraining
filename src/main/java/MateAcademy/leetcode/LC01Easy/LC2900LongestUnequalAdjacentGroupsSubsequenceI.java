0356ccbec94cb9805024e9a6733f41245d386d49
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-05-15 18:30:14 +0300

    working commit

--- src/main/java/MateAcademy/leetcode/LC01Easy/LC2900LongestUnequalAdjacentGroupsSubsequenceI.java
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC2900LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -8,6 +8,12 @@ import java.util.List;
  */
 
 public class LC2900LongestUnequalAdjacentGroupsSubsequenceI {
+
+
+
+
+
+
     public static void main(String[] args) {
         System.out.println(getLongestSubsequence(
                 new String[]{"e", "a", "b"},
