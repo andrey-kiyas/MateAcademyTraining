93f33c803d1457c97ad3a12b41a3ff89a03c385a
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-06-01 22:31:24 +0300

    working commit

--- src/main/java/MateAcademy/leetcode/LC02Medium/LC2929DistributeCandiesAmongChildrenII.java
+++ src/main/java/MateAcademy/leetcode/LC02Medium/LC2929DistributeCandiesAmongChildrenII.java
@@ -11,7 +11,7 @@ public class LC2929DistributeCandiesAmongChildrenII {
     }
 
     public static long distributeCandies(int n, int limit) {
-        long total = comb(n + 2, 2);
+        long total = comb(n + 2);
 
         for (int i = 1; i <= 3; i++) {
             for (int mask = 1; mask < (1 << 3); mask++) {
@@ -19,7 +19,7 @@ public class LC2929DistributeCandiesAmongChildrenII {
                 int sumOver = i * (limit + 1);
                 int remaining = n - sumOver;
                 if (remaining < 0) continue;
-                long subtract = comb(remaining + 2, 2);
+                long subtract = comb(remaining + 2);
                 if (i % 2 == 1) total -= subtract;
                 else total += subtract;
             }
@@ -28,8 +28,8 @@ public class LC2929DistributeCandiesAmongChildrenII {
         return total;
     }
 
-    private static long comb(int n, int k) {
-        if (n < k) return 0;
+    private static long comb(int n) {
+        if (n < 2) return 0;
         return (long) n * (n - 1) / 2;
     }
 }
