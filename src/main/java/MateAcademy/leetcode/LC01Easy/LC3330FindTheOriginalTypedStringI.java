2a9e2628a2572aaee5d16fc309216aed2fbd19eb
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-07-01 23:20:26 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC3330FindTheOriginalTypedStringI.java
@@ -0,0 +1,24 @@
+package MateAcademy.leetcode.LC01Easy;
+
+/**
+ * https://leetcode.com/problems/find-the-original-typed-string-i
+ */
+
+public class LC3330FindTheOriginalTypedStringI {
+    public static void main(String[] args) {
+        System.out.println(possibleStringCount("abbcccc")); // 5
+        System.out.println(possibleStringCount("abcd"));    // 1
+        System.out.println(possibleStringCount("aaaa"));    // 4
+    }
+
+    public static int possibleStringCount(String word) {
+        int count = 1;
+        int n = word.length();
+        for (int i = 1; i < n; i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
