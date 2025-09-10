9d7cd65d495bd879eb2739d48c52f6a1984999b3
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-09-10 22:12:24 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC02Medium/LC1733MinimumNumberOfPeopleToTeach.java
@@ -0,0 +1,68 @@
+package MateAcademy.leetcode.LC02Medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-people-to-teach
+ */
+
+public class LC1733MinimumNumberOfPeopleToTeach {
+    public static void main(String[] args) {
+        int n1 = 2;
+        int[][] languages1 = {{1}, {2}, {1, 2}};
+        int[][] friendships1 = {{1, 2}, {1, 3}, {2, 3}};
+        System.out.println(minimumTeachings(n1, languages1, friendships1)); // 1
+
+        int n2 = 3;
+        int[][] languages2 = {{2}, {1, 3}, {1, 2}, {3}};
+        int[][] friendships2 = {{1, 4}, {1, 2}, {3, 4}, {2, 3}};
+        System.out.println(minimumTeachings(n2, languages2, friendships2)); // 2
+    }
+
+    public static int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        int m = languages.length;
+        List<Set<Integer>> userLangs = new ArrayList<>();
+        for (int[] langList : languages) {
+            Set<Integer> set = new HashSet<>();
+            for (int l : langList) set.add(l);
+            userLangs.add(set);
+        }
+
+        Set<Integer> needTeach = new HashSet<>();
+        for (int[] f : friendships) {
+            int u = f[0] - 1, v = f[1] - 1;
+            Set<Integer> langsU = userLangs.get(u);
+            Set<Integer> langsV = userLangs.get(v);
+
+            boolean canCommunicate = false;
+            for (int l : langsU) {
+                if (langsV.contains(l)) {
+                    canCommunicate = true;
+                    break;
+                }
+            }
+            if (!canCommunicate) {
+                needTeach.add(u);
+                needTeach.add(v);
+            }
+        }
+
+        if (needTeach.isEmpty()) return 0;
+
+        int minTeach = Integer.MAX_VALUE;
+        for (int lang = 1; lang <= n; lang++) {
+            int count = 0;
+            for (int user : needTeach) {
+                if (!userLangs.get(user).contains(lang)) {
+                    count++;
+                }
+            }
+            minTeach = Math.min(minTeach, count);
+        }
+
+        return minTeach;
+    }
+}
