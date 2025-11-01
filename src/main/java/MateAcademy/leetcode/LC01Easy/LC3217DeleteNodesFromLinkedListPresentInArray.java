fb6c15875bb6b21589d6550b614732e2923724fe
Andrey Kiyas <andrey.kiyas@gmail.com>
2025-11-02 00:05:29 +0200

    working commit

--- src/main/java/MateAcademy/leetcode/LC01Easy/LC3217DeleteNodesFromLinkedListPresentInArray.java
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC3217DeleteNodesFromLinkedListPresentInArray.java
@@ -8,6 +8,20 @@ import java.util.Set;
  */
 
 public class LC3217DeleteNodesFromLinkedListPresentInArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        ListNode head1 = makeList(new int[]{1, 2, 3, 4, 5});
+        System.out.println("Example 1: " + listToString(modifiedList(nums1, head1))); // [4, 5]
+
+        int[] nums2 = {1};
+        ListNode head2 = makeList(new int[]{1, 2, 1, 2, 1, 2});
+        System.out.println("Example 2: " + listToString(modifiedList(nums2, head2))); // [2, 2, 2]
+
+        int[] nums3 = {5};
+        ListNode head3 = makeList(new int[]{1, 2, 3, 4});
+        System.out.println("Example 3: " + listToString(modifiedList(nums3, head3))); // [1, 2, 3, 4]
+    }
+
     public static ListNode modifiedList(int[] nums, ListNode head) {
         Set<Integer> s = new HashSet<>();
         for (int x : nums) {
@@ -25,6 +39,27 @@ public class LC3217DeleteNodesFromLinkedListPresentInArray {
         }
 
         return dummy.next;
+    }
+
+    static ListNode makeList(int[] arr) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for (int x : arr) {
+            cur.next = new ListNode(x);
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    static String listToString(ListNode head) {
+        StringBuilder sb = new StringBuilder("[");
+        while (head != null) {
+            sb.append(head.val);
+            if (head.next != null) sb.append(", ");
+            head = head.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
 
