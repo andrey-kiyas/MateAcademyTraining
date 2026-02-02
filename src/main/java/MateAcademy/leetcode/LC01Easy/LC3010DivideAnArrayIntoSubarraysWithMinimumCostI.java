0cbcd0fd6e111b169a09a88acc1c2f16b14a2e85
Andrey Kiyas <andrey.kiyas@gmail.com>
2026-02-02 23:01:05 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC3010DivideAnArrayIntoSubarraysWithMinimumCostI.java
@@ -0,0 +1,34 @@
+package MateAcademy.leetcode.LC01Easy;
+
+/**
+ * https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/description
+ */
+
+public class LC3010DivideAnArrayIntoSubarraysWithMinimumCostI {
+    public static void main(String[] args) {
+        System.out.println(minimumCost(new int[]{1, 2, 3, 12}));
+        System.out.println(minimumCost(new int[]{5, 4, 3}));
+
+        // Example 3: [10, 3, 1, 1] -> 12
+        System.out.println(minimumCost(new int[]{10, 3, 1, 1}));
+    }
+
+    public static int minimumCost(int[] nums) {
+        int n = nums.length;
+        int firstCost = nums[0];
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return firstCost + min1 + min2;
+    }
+}
