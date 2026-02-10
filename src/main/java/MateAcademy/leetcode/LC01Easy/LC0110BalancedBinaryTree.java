23648a8ba8e2b364947e5c11d5095463b971e3ab
Andrey Kiyas <andrey.kiyas@gmail.com>
2026-02-10 23:49:20 +0200

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC0110BalancedBinaryTree.java
@@ -0,0 +1,51 @@
+package MateAcademy.leetcode.LC01Easy;
+
+/**
+ * https://leetcode.com/problems/balanced-binary-tree
+ */
+
+public class LC0110BalancedBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7))
+        );
+
+        System.out.println(isBalanced(root)); // true
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private static int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
