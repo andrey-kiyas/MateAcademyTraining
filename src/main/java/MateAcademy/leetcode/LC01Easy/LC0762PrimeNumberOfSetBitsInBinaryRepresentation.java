fca47dcdf3ac37272743f5159c928d04884b635b
Andrey Kiyas <andrey.kiyas@gmail.com>
2026-02-22 15:58:01 +0200

    working commit

--- src/main/java/MateAcademy/leetcode/LC01Easy/LC0762PrimeNumberOfSetBitsInBinaryRepresentation.java
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC0762PrimeNumberOfSetBitsInBinaryRepresentation.java
@@ -22,7 +22,6 @@ public class LC0762PrimeNumberOfSetBitsInBinaryRepresentation {
     }
 
     private static boolean isPrime(int n) {
-        // Оскільки n <= 20 (для чисел до 10^6), ми можемо використати швидку перевірку
         return n == 2 || n == 3 || n == 5 || n == 7 || n == 11
                 || n == 13 || n == 17 || n == 19;
     }
