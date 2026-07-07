e1c73df16558c3979a6d2c84122c5276125d3401
Andrey Kiyas <andrey.kiyas@gmail.com>
2026-07-07 22:47:58 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC3754ConcatenateNonZeroDigitsAndMultiplyBySumI.java
@@ -0,0 +1,34 @@
+package MateAcademy.leetcode.LC01Easy;
+
+/**
+ * https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i
+ */
+
+public class LC3754ConcatenateNonZeroDigitsAndMultiplyBySumI {
+    public static void main(String[] args) {
+        System.out.println(sumAndMultiply(10203004)); // 12340
+        System.out.println(sumAndMultiply(1000));     // 1
+    }
+
+    public static long sumAndMultiply(int n) {
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder();
+        long sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch != '0') {
+                sb.append(ch);
+                sum += (ch - '0');
+            }
+        }
+
+        if (sb.length() == 0) {
+            return 0;
+        }
+
+        long x = Long.parseLong(sb.toString());
+
+        return x * sum;
+    }
+}
