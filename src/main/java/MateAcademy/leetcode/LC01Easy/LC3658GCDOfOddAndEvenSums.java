1b15d505256aa9d1f66a31a35fb55be5bdbc1873
Andrey Kiyas <andrey.kiyas@gmail.com>
2026-07-15 21:17:57 +0300

    working commit

---
+++ src/main/java/MateAcademy/leetcode/LC01Easy/LC3658GCDOfOddAndEvenSums.java
@@ -0,0 +1,16 @@
+package MateAcademy.leetcode.LC01Easy;
+
+/**
+ * https://leetcode.com/problems/gcd-of-odd-and-even-sums
+ */
+
+public class LC3658GCDOfOddAndEvenSums {
+    public static void main(String[] args) {
+        System.out.println(gcdOfOddEvenSums(4)); // 4
+        System.out.println(gcdOfOddEvenSums(5)); // 5
+    }
+
+    public static int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
